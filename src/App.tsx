5534c6cefdd11b8b038e6e51cf428c9a75b7cce0
Nazim Saifi <saifinazim2311@gmail.com>
2026-05-06 00:58:27 +0530

    added blur bubble

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+  
+
+  return (
+    <main className="bg-gray-50 h-screen text-gray-950">
+      <div className="bg-gray-primary absolute h-124 w-124 rounded-full -top-20 left-20 blur-[10rem] -z-10"></div>
+      <div className="bg-blue-primary absolute h-130 w-124 rounded-full -top-10 right-20 blur-[10rem] -z-10"></div>
+     
+    </main>
+  );
+}
+
+export default App

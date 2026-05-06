2c8cc6414590f11fd35afa5b18b097f68f3a43f9
Nazim Saifi <saifinazim2311@gmail.com>
2026-05-06 19:45:19 +0530

    navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
   
 
   return (
-    <main className="bg-gray-50  text-gray-950">
-      <div className="bg-gray-primary absolute h-124 w-124 rounded-full -top-20 left-20 blur-[10rem] -z-10"></div>
-      <div className="bg-blue-primary absolute h-130 w-124 rounded-full -top-10 right-20 blur-[10rem] -z-10"></div>
+    <main className="text-gray-950">
+      <div className="bg-gray-primary absolute h-124 w-124 rounded-full -top-20 left-20 blur-[10rem] "></div>
+      <div className="bg-blue-primary absolute h-130 w-124 rounded-full -top-10 right-20 blur-[10rem] "></div>
       <Header/>
     </main>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,28 @@
 import {motion } from 'motion/react'
 
+
 const Header = () => {
   return (
-   
-      <motion.div className="transition-all duration-500 fixed top-0 w-full -translate-x-1/2 h-10 bg-white/50 border border-white/80 shadow-md shadow-black/20  backdrop-blur-3xl sm:top-4 sm:left-1/2 sm:rounded-full sm:w-140 flex justify-between items-center px-3"
+    
+      <motion.div className="fixed left-1/2 -translate-x-1/2 top-0 rounded-none h-10 bg-white/50 border border-white/80 shadow-md shadow-black/20  backdrop-blur-3xl sm:top-4 sm:rounded-full sm:w-140 flex justify-between items-center px-3 w-full "
       initial={{y:-100,opacity:0}}
       animate={{y:0,opacity:1}}
+      transition={{delay:1}}
       >
-        {[1,2,3,4,5,6].map(()=>{
-            return (
-              <motion.div
-                initial={{ y: -100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1  }}
-               
-                className="transition-all duration-300 cursor-pointer hover:bg-gray-200 text-gray-500 hover:text-black rounded-full px-4 py-1"
-              >
-                About
-              </motion.div>
-            );
+        {["Home", "About", "Contact", "Projects", "Skills", "Expirience"].map((d) => {
+          return (
+            <motion.div
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.75 }}
+              className="transition-all duration-300 cursor-pointer hover:bg-gray-200 text-gray-500 hover:text-black rounded-full px-4 py-1"
+            >
+              {d}
+            </motion.div>
+          );
         })}
       </motion.div>
-     
+    
   );
 }
 
